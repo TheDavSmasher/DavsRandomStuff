982bca40a70070d255594e6b9e873e04d00c5b88
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-02-28 16:50:27 -0700

    Added flip/rotate

--- Loenn/entities/dangerDashSpring.lua
+++ Loenn/entities/dangerDashSpring.lua
@@ -3,6 +3,46 @@ local utils = require("utils")
 local modName = "DavsRandomStuff"
 local springType =  modName .. "/DangerDashSpring"
 
+
+local springOpts = { "Up", "Left", "Down", "Right" }
+
+local function getOrientation(entity, offset)
+  local index = 1
+  for i,v in ipairs(springOpts) do
+    if (springType .. v) == entity._name then
+      index = i
+      break
+    end
+  end
+  if offset == nil then
+    return index
+  end
+  return springOpts[(index + offset - 1) % 4 + 1]
+end
+
+local function rotate(room, entity, direction)
+    local dir = getOrientation(entity)
+    local swap = entity.width
+    entity.height = entity.width
+    entity.width = swap
+    entity._name = getOrientation(entity, direction)
+    return true
+end
+
+local function flip(room, entity, horizontal, vertical)
+  if getOrientation(entity) % 2 == 0 then
+    if horizontal then
+      entity._name = getOrientation(entity, 2)
+    end
+    return horizontal
+  end
+
+  if vertical then
+    entity._name = getOrientation(entity, 2)
+  end
+  return vertical
+end
+
 local function getPlacement(name, rotation, xOff, yOff, x, y)
   local spring = {}
 
@@ -13,6 +53,8 @@ local function getPlacement(name, rotation, xOff, yOff, x, y)
   spring.justification = {0.5, 1.0}
   spring.texture = spritePath .. "00"
   spring.rotation = rotation
+  spring.flip = flip
+  spring.rotate = rotate
   spring.selection = function (room, entity)
     return utils.rectangle(entity.x - xOff, entity.y - yOff, x, y)
   end
